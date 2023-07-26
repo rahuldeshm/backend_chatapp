a6f42e68d3f1f375d5dfa98e36d0880b6cb16b50
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-07-26 17:57:17 +0530

    login route added

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,17 +42,55 @@ exports.signup = (req, res, next) => {
 
           res.json({
             message: "Login Successful",
-            userame: result.username,
+            username: result.username,
             email: req.body.email,
             phone: result.phone,
             token: token,
           });
         } catch (err) {
-          res.status(403).json({ message: "Email already Present" });
+          res
+            .status(403)
+            .json({ message: "EMAIL ALREADY PRESENT PLEASE LOGIN" });
         }
       });
     }
   } catch (err) {
-    res.status(403).json({ message: "Unknown error occured in backend" });
+    res.status(403).json({ message: "UNKNOWN ERROR" });
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  try {
+    const result = await User.findOne({ where: { email: req.body.email } });
+    if (result) {
+      bcrypt.compare(
+        req.body.password,
+        result.password,
+        async (err, response) => {
+          if (err) {
+            throw new Error("SOMETHING WENT WRONG");
+          }
+          if (response === true) {
+            const token = jwt.sign(
+              { id: result.id, username: result.username },
+              process.env.TOKEN_KEY
+            );
+            res.json({
+              message: "Login Successful",
+              username: result.username,
+              phone: result.phone,
+              email: req.body.email,
+              token: token,
+            });
+          } else {
+            res.status(401).json({ message: "USER UNAUTHORIZED" });
+          }
+        }
+      );
+    } else {
+      throw new Error("USER NOT FOUND");
+    }
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth");
 const authRoutes = express.Router();
 
 authRoutes.use("/signup", authController.signup);
+authRoutes.use("/signin", authController.signin);
 
 module.exports = authRoutes;
