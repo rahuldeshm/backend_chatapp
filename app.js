8f326b4d1d9c9789a4ff0b7c819708728a5ae330
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-07-26 14:51:15 +0530

    added signup controller

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+require("dotenv").config();
 const authRoutes = require("./routes/auth");
+const sequelize = require("./util/db");
 
 const app = express();
 
@@ -9,3 +11,7 @@ app.use(bodyParser.json({ extended: true }));
 app.use("/auth", authRoutes);
 
 app.listen(3000);
+
+sequelize.sync().then(() => {
+  app.listen(4000);
+});
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,58 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+function isStringInvalid(string) {
+  if (string == undefined || string.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 exports.signup = (req, res, next) => {
   console.log(req.body);
+  try {
+    const { username, email, phone, password } = req.body;
+    console.log(username, email, phone, password);
+    if (
+      isStringInvalid(email) ||
+      isStringInvalid(password) ||
+      isStringInvalid(phone) ||
+      isStringInvalid(username)
+    ) {
+      return res.status(400).json({ err: "bad request. something is missing" });
+    } else {
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        try {
+          const result = await User.create({
+            username,
+            email,
+            phone,
+            password: hash,
+          });
+
+          const token = jwt.sign(
+            { id: result.id, username: result.username },
+            process.env.TOKEN_KEY
+          );
+          console.log(token, result.id, result.username);
+          res.json({
+            message: "Login Successful",
+            userame: result.username,
+            email: req.body.email,
+            phone: result.phone,
+            token: token,
+          });
+        } catch (err) {
+          res.status(403).json(err);
+        }
+      });
+    }
+  } catch (err) {
+    res.status(403).json(err);
+  }
 };
